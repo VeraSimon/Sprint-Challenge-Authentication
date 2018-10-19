6697a09e3622e34c37195858635d0443ff648288
Vera Simon <ghjkl>
2018-10-19 10:06:17 -0700

    Sprint Challenge - Authentication - stretch
    Finished /login route for client

--- client-thingy/src/components/Login/login.js
+++ client-thingy/src/components/Login/login.js
@@ -1,11 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Axios from 'axios';
 
-const Login = () => {
-	return (
-		<div>
-			<p>Hi!</p>
-		</div>
-	);
-};
+class Login extends Component {
+	state = {
+		username: '',
+		password: '',
+		error: {},
+		success: false
+	};
+
+	loginUser = (event) => {
+		event.preventDefault();
+		const endpoint = 'http://localhost:3300/api/login';
+		const creds = {
+			username: this.state.username,
+			password: this.state.password
+		};
+		Axios.post(endpoint, creds)
+			.then((res) => {
+				localStorage.setItem('userToken', res.data.token);
+				this.setState({ ...this.state, success: true });
+			})
+			.catch((err) => {
+				console.error('Error:\n', err.response.data);
+				this.setState({
+					...this.state,
+					error: err.response.data
+				});
+			});
+	};
+
+	inputHandler = (event) => {
+		this.setState({
+			...this.state,
+			[event.target.name]: event.target.value
+		});
+	};
+
+	render() {
+		if (this.state.success === true) {
+			return <Redirect to="/jokes" />;
+		}
+		return (
+			<div>
+				<form onSubmit={this.loginUser}>
+					<label htmlFor="username">
+						Username:
+						<input
+							name="username"
+							type="text"
+							value={this.state.username}
+							onChange={this.inputHandler}
+						/>
+					</label>
+					<br />
+					<label htmlFor="password">
+						Password:
+						<input
+							name="password"
+							type="password"
+							value={this.state.password}
+							onChange={this.inputHandler}
+						/>
+					</label>
+					<br />
+					<input type="submit" value="Login" />
+				</form>
+			</div>
+		);
+	}
+}
 
 export default Login;
