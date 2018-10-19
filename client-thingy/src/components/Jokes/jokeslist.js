e2a7f915a5ebd545c08325799b03d463cf694dff
Vera Simon <ghjkl>
2018-10-19 10:31:43 -0700

    Sprint Challenge - Authentication - stretch
    Finished JokesList

--- client-thingy/src/components/Jokes/jokeslist.js
+++ client-thingy/src/components/Jokes/jokeslist.js
@@ -1,13 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Axios from 'axios';
 
 import Joke from './joke';
 
-const JokesList = () => {
-	return (
-		<div>
-			<p>Hi!</p>
-		</div>
-	);
-};
+class JokesList extends Component {
+	state = {
+		jokes: [],
+		error: {}
+	};
+
+	componentDidMount = () => {
+		const endpoint = 'http://localhost:3300/api/jokes';
+		const token = localStorage.getItem('userToken');
+		const options = {
+			headers: {
+				Authorization: token
+			}
+		};
+
+		Axios.get(endpoint, options)
+			.then((res) => {
+				this.setState({
+					...this.state,
+					jokes: res.data
+				});
+			})
+			.catch((err) => {
+				// console.error('Error:\n', err.response);
+				this.setState({
+					...this.state,
+					error: err.response
+				});
+			});
+	};
+
+	render() {
+		return (
+			<div>
+				{this.state.error && this.state.error.status ? (
+					<p>{`HTTP ${this.state.error.status}: ${
+						this.state.error.statusText
+					}`}</p>
+				) : (
+					this.state.jokes.map((joke) => (
+						<Joke joke={joke} key={joke.id * Math.random()} />
+					))
+				)}
+			</div>
+		);
+	}
+}
 
 export default JokesList;
